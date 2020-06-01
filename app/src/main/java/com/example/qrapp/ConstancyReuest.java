27e9169f151b34e84e7784ce73b765058d296ecf
under305 <underjoel30@gmail.com>
2020-06-01 00:16:41 -0500

    ya recibe

--- app/src/main/java/com/example/qrapp/ConstancyReuest.java
+++ app/src/main/java/com/example/qrapp/ConstancyReuest.java
@@ -1,4 +1,24 @@
 package com.example.qrapp;
 
-public class ConstancyReuest {
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConstancyReuest extends StringRequest {
+
+    public static String url = "http://192.168.1.75/constancias-tec/Controllers/mainController.php?opt=consultConstancy";
+    private Map<String, String> params;
+    public ConstancyReuest(String cadena, Response.Listener<String> listener){
+        super(Method.POST, url, listener, null);
+        params = new HashMap<>();
+        params.put("identifier", "identificadordepruebas");
+        params.put("id", cadena);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
 }
--- app/src/main/java/com/example/qrapp/MainActivity.java
+++ app/src/main/java/com/example/qrapp/MainActivity.java
@@ -10,8 +10,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -34,6 +40,27 @@ public class MainActivity extends AppCompatActivity {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if(result != null)
             if (result.getContents() != null){
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            if(jsonResponse.getString("response").equals("OK")){
+                                Toast.makeText(thisContext, "Todo bien ", Toast.LENGTH_LONG).show();
+
+                            }else if(jsonResponse.getString("response").equals("IDENT")){
+                                Toast.makeText(thisContext, "Todo mal", Toast.LENGTH_LONG).show();
+                            }else{
+                                Toast.makeText(thisContext, "Todo peor", Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                ConstancyReuest constancyReuest = new ConstancyReuest(result.getContents(), responseListener);
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                queue.add(constancyReuest);
                 Toast.makeText(thisContext, "El código es:  "+result.getContents(), Toast.LENGTH_LONG).show();
             }else{
                 Toast.makeText(thisContext, "Error al leer el código", Toast.LENGTH_LONG).show();
