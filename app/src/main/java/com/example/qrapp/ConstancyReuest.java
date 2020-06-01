4e3c5052c0e9878414e1efe94a02ff0357cff9d0
under305 <underjoel30@gmail.com>
2020-05-31 22:42:24 -0500

    app agregada

---
+++ app/src/main/java/com/example/qrapp/ConstancyReuest.java
@@ -0,0 +1,4 @@
+package com.example.qrapp;
+
+public class ConstancyReuest {
+}
---
+++ app/src/main/java/com/example/qrapp/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.qrapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button boton;
+    public static Context thisContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        boton = findViewById(R.id.boton);
+        thisContext = this;
+        boton.setOnClickListener(mOnClickListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null)
+            if (result.getContents() != null){
+                Toast.makeText(thisContext, "El código es:  "+result.getContents(), Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(thisContext, "Error al leer el código", Toast.LENGTH_LONG).show();
+            }
+    }
+
+    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.boton:
+                    new IntentIntegrator(MainActivity.this).initiateScan();
+                    break;
+            }
+        }
+    };
+}
